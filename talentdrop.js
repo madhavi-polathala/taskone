03574cfaabf1a72d8b93157af4c01ae19da211a9
madhavi-polathala <140619323+madhavi-polathala@users.noreply.github.com>
2023-08-03 18:58:53 +0530

    Add files via upload

---
+++ talentdrop.js
@@ -0,0 +1,6 @@
+let bar=document.querySelector("#bars");
+let nav=document.querySelector(".nav-bar");
+function toggleMenu(){
+    bar.classList.toggle('.active');
+    nav.classList.toggle('nav-bar');
+}
